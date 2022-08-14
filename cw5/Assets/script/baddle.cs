3804d547a2a2282d2ac45a815c4029424c343c73
ThamerAsaad <109876113+ThamerAsaad@users.noreply.github.com>
2022-08-14 16:34:51 +0300

    cw5

---
+++ cw5/Assets/script/baddle.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class baddle : MonoBehaviour
+{
+    Rigidbody2D RB;
+    float playerInput;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        playerInput = Input.GetAxis("Horizontal");
+        RB.velocity = new Vector2(playerInput * speed,0);
+    }
+ 
+}
